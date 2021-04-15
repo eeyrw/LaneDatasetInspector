c660d596122911b1be1a5e8f4097da804139b6b7
Sandeep Menon <sandeepmenon@deepen.ai>
2021-04-15 15:46:22 +0530

    added Bezier spline evaluation from author source

--- CaltechLanesDbInspector.py
+++ CaltechLanesDbInspector.py
@@ -2,9 +2,7 @@ import scipy.io as sio
 import numpy as np
 import cv2
 import os
-from tqdm import tqdm
 import argparse
-from scipy.interpolate import interp1d
 
 
 def walkThroughDataset(dataSetDir):
@@ -30,29 +28,86 @@ def walkThroughDataset(dataSetDir):
                     type = label['type']
                     if type == 'spline':
                         points = label['points']
-                        y_coords = points[:,1]
-                        if len(y_coords) != len(set(y_coords)):
-                            # If multiple x values, cubic interpolation fails. Plotting only the 4 points in such cases
-                            for point in points:
-                                coord = (int(point[0]), int(point[1]))
-                                cv2.circle(img, coord, 5, subtype_colors[subtype], thickness=-1, lineType=8)
-                            cv2.imwrite(f"{points}.png",img)
-                        else:
-                            x_coords = points[:,0]
-                            spline_function = interp1d(y_coords, x_coords, kind='cubic')
-                            y_min, y_max = min(y_coords), max(y_coords)
-                            ynew = np.arange(y_min, y_max, 1)
-                            xnew = spline_function(ynew)
-                            points_new = [(x,y) for x,y in zip(xnew, ynew)]
-                            for point in points_new:
-                                coord = (int(point[0]), int(point[1]))
-                                cv2.circle(img, coord, 5, subtype_colors[subtype], thickness=-1, lineType=8)
+                        points_new = get_lane_points(spline=points, interval=0.02)
+
+                        # points_new = [(x,y) for x,y in zip(xnew, ynew)]
+                        for point in points_new:
+                            coord = (int(point[0]), int(point[1]))
+                            cv2.circle(img, coord, 4, subtype_colors[subtype], thickness=-1, lineType=8)
 
             cv2.imshow('Caltech Lanes Dataset Quick Inspector', img)
             k = cv2.waitKey(1) & 0xff
             if k == 27:
                 break
             
+
+
+def get_lane_points(spline, interval=0.05):
+    """
+    Function to evaluate a Bezier spline with the specified degree.
+    Algorithm taken from the matlab code: https://github.com/mohamedadaly/caltechcv-image-labeler/blob/master/ccvEvalBezSpline.m
+
+    Parameters
+    ----------
+    spline    - input 3 or 4 points in a matrix 3x2 or 4x2 [xs, ys]
+    interval         - [0.05] the interval to use for evaluation
+
+    Returns
+    -------
+    output points nx2 [xs; ys]
+    
+    """
+
+    degree = spline.shape[0] - 1
+    n = int(np.floor(1/interval) + 1)
+    if degree == 2:
+        M =	np.array([[1, -2, 1], [-2,2,0], [1,0,0]])
+        abcd= np.matmul(M, spline)
+        a = abcd[0]
+        b = abcd[1]
+        c = abcd[2]
+
+        P = c
+        dP = b*interval + a*interval**2
+        ddP = 2*a*interval**2
+
+        outPoints = np.zeros((n, spline.shape[1]))
+        outPoints[0,:] = P
+        for i in range(1,n):
+            # calculate new point
+            P = P + dP
+            # update steps
+            dP = dP + ddP
+            # store in output array
+            outPoints[i,:] = P
+    elif degree == 3:
+        M =	np.array([[-1, 3, -3,1], [3,-6,3,0], [-3,3,0,0], [1,0,0,0]])
+        abcd= np.matmul(M, spline)
+        a = abcd[0]
+        b = abcd[1]
+        c = abcd[2]
+        d = abcd[3]
+
+        P = d
+        dP = c*interval + b * interval**2 + a*interval**3
+        ddP = 2*b*interval**2 + 6*a*interval**3
+        dddP = 6*a*interval**3
+
+        outPoints = np.zeros((n, spline.shape[1]))
+        outPoints[0,:] = P
+        for i in range(1,n):
+			# calculate new point
+            P = P + dP
+			# update steps
+            dP = dP + ddP
+            ddP = ddP + dddP
+			# store in output array
+            outPoints[i,:] = P
+    else:
+        raise NotImplementedError(f"Supports Bezier spline with degrees 2 or 3, but got {degree}")
+		
+    return outPoints
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
